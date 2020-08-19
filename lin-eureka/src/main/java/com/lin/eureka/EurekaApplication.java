03900c9bd749ba76ee29774b5b96afba0f72aeea
shen <zguishen@foxmail.com>
2020-08-19 23:50:47 +0800

    feat：add

---
+++ lin-eureka/src/main/java/com/lin/eureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.lin.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author nathan
+ * @date 2020/8/19 23:06
+ * @desc EurekaApplication
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
+
